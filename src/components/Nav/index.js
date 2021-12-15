9659ef8cfb090d5cda81d18bc13c51284f9c91f6
James K <projdk90@gmail.com>
2021-12-14 21:04:47 -0800

    beginning of the module of display the photos

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,11 +2,15 @@ import React, { useEffect } from "react";
 import { capitalizeFirstLetter } from "../../utils/helpers";
 
 function Nav(props) {
-  useEffect(() => {
-    document.title = capitalizeFirstLetter(currentCategory.name);
-  }, [currentCategory]);
+  //useEffect(() => {
+  //document.title = capitalizeFirstLetter(currentCategory.name);
+  //}, [currentCategory]);
 
   const { categories = [], setCurrentCategory, currentCategory } = props;
+  const handleClick = (item) => {
+    console.log(item);
+    return item;
+  };
 
   return (
     <header className="flex-row px-1">
