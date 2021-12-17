b741821ea3762dafbf17b301ced7239a5a6dc97a
James K <projdk90@gmail.com>
2021-12-17 14:22:56 -0800

    control state with a controlled component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import ContactForm from "./components/Contact";
 import Nav from "./components/Nav";
 import About from "./components/About";
 import Gallery from "./components/Gallery";
@@ -30,6 +31,9 @@ function App() {
         currentCategory={currentCategory}
       ></Nav>
       <main>
+        <ContactForm></ContactForm>
+        <Gallery currentCategory={currentCategory}></Gallery>
+        <About></About>
         <Gallery currentCategory={currentCategory}></Gallery>
         <About></About>
       </main>
